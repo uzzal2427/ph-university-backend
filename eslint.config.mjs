f87b54ef52da1b074eb749a03937331a412a3128
uzzal2427 <uzzal2427ahmed@gmail.com>
2024-08-09 01:45:13 +0300

    ph university backend project setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+    },
+  },
+  {
+    ignores: ["**/node_modules", "**/dist/"],
+  }
+);
